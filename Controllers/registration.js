60f43079199622eec600838d3edc0ee9512598c3
Niloy <muntasirniloy2002@gmail.com>
2025-07-13 16:46:51 +0600

    first version

--- Controllers/registration.js
+++ Controllers/registration.js
@@ -111,8 +111,8 @@ exports.login = async (req, res) => {
     
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict", 
+      secure: "production",
+      sameSite: "None", 
       maxAge: 7 * 24 * 60 * 60 * 1000, 
     });
 
