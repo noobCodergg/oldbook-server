8c9a61b284b8a61e31e128524fe58f8f8ccc64f9
Niloy <muntasirniloy2002@gmail.com>
2025-07-13 17:58:29 +0600

    first version

--- Controllers/registration.js
+++ Controllers/registration.js
@@ -150,8 +150,8 @@ exports.logout = async (req, res) => {
   try {
     res.clearCookie('token', {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      secure: 'production',
+      sameSite: 'None',
     });
 
     return res.status(200).json({ message: 'Logged out successfully' });
