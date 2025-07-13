c8c50b95c5c17ca4595c084755cc290d82fd0bbf
Niloy <muntasirniloy2002@gmail.com>
2025-07-13 15:37:41 +0600

    first version

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "https://glittery-pudding-a04bf4.netlify.app/", 
     methods: "GET,POST,PUT,DELETE",
     credentials: true, 
   })
